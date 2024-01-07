813a8250ac785aad5a87dbaae8fe657961d5d513
genki utsumi <zabanya0770@gmail.com>
2024-01-08 00:55:31 +0900

    add: cssを付与

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,13 @@ type SquareProps = {
 const Square: React.FC<SquareProps> = ({value, handleClick}) => {
   return(
     <>
-      <button onClick={handleClick}>{value}</button>
+      <button className='square' onClick={handleClick}>{value}</button>
     </>
   )
 }
 
 const Board = () => {
-  
+
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   const handleClick = (i: number) => {
@@ -26,17 +26,17 @@ const Board = () => {
 
   return(
     <>
-      <div>
+      <div className='board-row'>
         <Square value={squares[0]} handleClick={() => handleClick(0)}/>
         <Square value={squares[1]} handleClick={() => handleClick(1)}/>
         <Square value={squares[2]} handleClick={() => handleClick(2)}/>
       </div>
-      <div>
+      <div className='board-row'>
         <Square value={squares[3]} handleClick={() => handleClick(3)}/>
         <Square value={squares[4]} handleClick={() => handleClick(4)}/>
         <Square value={squares[5]} handleClick={() => handleClick(5)}/>
       </div>
-      <div>
+      <div className='board-row'>
         <Square value={squares[6]} handleClick={() => handleClick(6)}/>
         <Square value={squares[7]} handleClick={() => handleClick(7)}/>
         <Square value={squares[8]} handleClick={() => handleClick(8)}/>
