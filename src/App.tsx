286feaa946656fd4dd5c71ad9f9d205483542206
genki utsumi <zabanya0770@gmail.com>
2024-01-08 00:50:29 +0900

    add: ボタンにクリックアクションを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,46 @@
+import { useState } from "react"
+
 type SquareProps = {
-  num: number
+  value: string
+  handleClick: () => void
 }
 
-const Square: React.FC<SquareProps> = ({num}) => {
+const Square: React.FC<SquareProps> = ({value, handleClick}) => {
   return(
     <>
-      <button>{num}</button>
+      <button onClick={handleClick}>{value}</button>
     </>
   )
 }
 
 const Board = () => {
+  
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const handleClick = (i: number) => {
+    const newSquares = squares.slice();
+    newSquares[i] = 'X'
+    setSquares(newSquares);
+    console.log('clicked')
+  }
+
   return(
     <>
-      <Square num={0}/>
+      <div>
+        <Square value={squares[0]} handleClick={() => handleClick(0)}/>
+        <Square value={squares[1]} handleClick={() => handleClick(1)}/>
+        <Square value={squares[2]} handleClick={() => handleClick(2)}/>
+      </div>
+      <div>
+        <Square value={squares[3]} handleClick={() => handleClick(3)}/>
+        <Square value={squares[4]} handleClick={() => handleClick(4)}/>
+        <Square value={squares[5]} handleClick={() => handleClick(5)}/>
+      </div>
+      <div>
+        <Square value={squares[6]} handleClick={() => handleClick(6)}/>
+        <Square value={squares[7]} handleClick={() => handleClick(7)}/>
+        <Square value={squares[8]} handleClick={() => handleClick(8)}/>
+      </div>
     </>
   )
 }
