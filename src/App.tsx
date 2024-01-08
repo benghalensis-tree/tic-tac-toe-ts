56e50b81b90482febd1de6316aa2bbe4c6a031a4
genki utsumi <zabanya0770@gmail.com>
2024-01-08 19:44:39 +0900

    add: 勝利

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ type BoardProps = {
 const Board: React.FC<BoardProps> = ({ squares, xIsNext, handlePlay }) => {
 
   const handleClick = (i: number) => {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
     const newSquares = squares.slice();
@@ -34,8 +34,17 @@ const Board: React.FC<BoardProps> = ({ squares, xIsNext, handlePlay }) => {
     handlePlay(newSquares);
   }
 
+  const winner = calculateWinner(squares);
+  let status: string;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   return(
     <>
+      {status}
       <div className='board-row'>
         <Square value={squares[0]} handleClick={() => handleClick(0)}/>
         <Square value={squares[1]} handleClick={() => handleClick(1)}/>
@@ -53,6 +62,26 @@ const Board: React.FC<BoardProps> = ({ squares, xIsNext, handlePlay }) => {
       </div>
     </>
   )
+}
+
+const calculateWinner = (squares: (string | null)[]) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 const Game = () => {
