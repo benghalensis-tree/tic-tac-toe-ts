06377367e3d45b8e76ab5be1baab1719db04dd6d
genki utsumi <zabanya0770@gmail.com>
2024-01-08 18:49:41 +0900

    add: 過去にジャンプできる機能追加

--- src/App.tsx
+++ src/App.tsx
@@ -55,16 +55,41 @@ const Board: React.FC<BoardProps> = ({ squares, xIsNext, handlePlay }) => {
 const Game = () => {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
 
   const handlePlay = (newSquares: (string | null)[]) => {
-    setHistory([...history, newSquares])
+    const nextHistory = [...history.slice(0, currentMove + 1),newSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length -1);
     setXIsNext(!xIsNext);
   }
+  const jumpTo = (nextMove: number) => {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+  }
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   return (
     <>
-      <Board squares={currentSquares} xIsNext={xIsNext} handlePlay={handlePlay}/>
+      <div>
+        <Board squares={currentSquares} xIsNext={xIsNext} handlePlay={handlePlay}/>
+      </div>
+      <div>
+        {moves}
+      </div>
     </>
   )
 }
